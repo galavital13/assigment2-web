e50fd4681f374922d6c7d60a9b025c9bb601e82c
galavital13 <102811365+galavital13@users.noreply.github.com>
2024-02-08 20:47:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const authMiddleware = require('./middleware/authMiddleware');
+const errorHandler = require('./errors/errorHandler');
+const connectDB = require('./db/db');
+const itemsRouter = require('./router/itemsRouter');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(authMiddleware); 
+app.use('/items', itemsRouter);
+app.use(errorHandler); 
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
